a7010f6cd3d30811e95d7e9689af7c8f0e0b392b
wyatt987 <526wbedor@frhsd.com>
2024-11-25 10:40:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let userName = prompt("Hello, What is your name?")
+//Input user name to website//
+userName ? document.getElementById('greeting').innerText=`Hello, ${userName}!` : document.getElementById('greeting').innerText=`Hello Stranger!`
+
+
+
+
+function shakeMagic8Ball(){
+    //Get User Question//
+var userQuestion = 'Am I a Good Driver?';
+console.log(`${userName} asked ${userQuestion}`);
+//DO NOT TOUCH THIS CODE!//
+const randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber)
+let eightBall = ''
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+    eightBall = 'It is decidedly so'
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+   eightBall = 'Cannot predict now'
+    break;
+  case 4:
+    eightBall = 'Do not count on it'
+    break;
+  case 5:
+    eightBall = 'My sources say no'
+    break;
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+    break;
+}
+console.log(`The eight ball answered:${eightBall}`);
+}
+
